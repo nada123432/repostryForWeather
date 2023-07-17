a12d8c557a497a4d9a884c7ff9baf5d80f581f54
nada123432 <139780545+nada123432@users.noreply.github.com>
2023-07-17 23:08:01 +0300

    Add files via upload

---
+++ WeatherProject/WeatherProject/Helpers/Settings.cs
@@ -0,0 +1,69 @@
+﻿// Helpers/Settings.cs
+using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace WeatherProject.Helpers
+{
+    /// <summary>
+    /// This is the Settings static class that can be used in your Core solution or in any
+    /// of your client applications. All settings are laid out the same exact way with getters
+    /// and setters. 
+    /// </summary>
+    public static class Settings
+    {
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        #region Setting Constants
+
+        private const string SettingsKey = "settings_key";
+        private const string LanguageKey = "Language_key";
+        private const string txtnameKey = "UserName_key";
+        private static readonly string SettingsDefault = string.Empty;
+
+        #endregion
+
+
+        public static string GeneralSettings
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue(SettingsKey, value);
+            }
+        }
+
+        public static string Language
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault(LanguageKey, "ar");
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue(LanguageKey, value);
+            }
+        }
+
+        public static string txtname
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault(txtnameKey, SettingsDefault);
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue(txtnameKey, value);
+            }
+        }
+
+    }
+}
