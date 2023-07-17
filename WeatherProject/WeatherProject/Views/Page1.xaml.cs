0c529928661a05483cef544b8c3cd8fa41403996
nada123432 <139780545+nada123432@users.noreply.github.com>
2023-07-17 23:06:38 +0300

    Add files via upload

---
+++ WeatherProject/WeatherProject/Views/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        
+        }
+       
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+      
+            // ...
+
+            if (string.IsNullOrEmpty(txtCountry.Text) || !Regex.IsMatch(txtCountry.Text, @"^[a-zA-Z]+$"))
+            {
+                await DisplayAlert("Information", "Enter a valid City", "Ok", "Cancel");
+                return;
+            }
+
+            Helpers.Settings.txtname = txtCountry.Text;
+            
+            await   Navigation.PushAsync(new MainPage());
+        }
+    }
+}
